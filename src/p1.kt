59642554957a44091f37ecc0f343ee3de5c3688f
Binduprajapati23 <binduprajapati2312@gmail.com>
2024-10-24 19:33:17 +0530

    commit

---
+++ src/p1.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val a: Int = 5
+    val b: Float = 1.5f
+    val c: Char = 'D'
+    val d: String = "bindu"
+    val f: Double = 96.47
+    val g: Long = 124
+    val h: Short = -2
+    val i: Byte = 27
+    println("Integer value is: $a")
+    println("Float value: $b")
+    println("Character value: $c")
+    println("String Value: $d")
+    println("Double value: $f")
+    println("Long value: $g")
+    println("Short value: $h")
+    println("Byte value: $i")
+}
